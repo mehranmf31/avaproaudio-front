5e40496575f9693a2647e042c0f77245a03531fc
Mehran <mehranmf31@gmail.com>
2023-02-13 12:30:34 +0330

    fix: modify appRoutes file

--- src/constants/appRoutes.ts
+++ src/constants/appRoutes.ts
@@ -1,19 +1,14 @@
-export const PAGE_ROUTES = {
-  MAIN: '/',
-  ABOUT: '/about',
-  CONTACT: '/contact',
-  DISTRIBUTORS: '/distributors',
-
-  CATEGORIES: '/categories',
-  CATEGORY: (slug: string) => `/categories/${slug}`,
-  PRODUCTS: (categorySlug: string, seriesSlug: string) =>
-    `/categories/${categorySlug}/${seriesSlug}`,
-  PRODUCT: (categorySlug: string, seriesSlug: string, productSlug: string) =>
-    `/categories/${categorySlug}/${seriesSlug}/${productSlug}`,
-
-  BLOG: '/blog',
-  BLOG_POST: (slug: string) => `/blog/${slug}`,
-
-  PROJECTS: '/projects',
-  PROJECT: (slug: string) => `/projects/${slug}`,
-};
+export const MAIN = '/';
+export const ABOUT = '/about';
+export const CONTACT = '/contact';
+export const DISTRIBUTORS = '/distributors';
+export const CATEGORIES = '/categories';
+export const CATEGORY = (slug: string) => `/categories/${slug}`;
+export const PRODUCTS = (categorySlug: string, seriesSlug: string) =>
+  `categories/${categorySlug}/${seriesSlug}`;
+export const PRODUCT = (categorySlug: string, seriesSlug: string, productSlug: string) =>
+  `categories/${categorySlug}/${seriesSlug}/${productSlug}`;
+export const BLOG = '/blog';
+export const BLOG_POST = (slug: string) => `/blog/${slug}`;
+export const PROJECTS = '/projects';
+export const PROJECT = (slug: string) => `/projects/${slug}`;

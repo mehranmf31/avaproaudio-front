b3e4801a4eb70a0031af6445588e411229de002b
Mehran <mehranmf31@gmail.com>
2023-02-13 18:49:23 +0330

    fix: add global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1.5rem',
+        xl: '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],

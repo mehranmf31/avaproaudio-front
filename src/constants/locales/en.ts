b4331ff066d3ea20347e2619aad29201691d5312
Mehran <mehranmf31@gmail.com>
2023-02-13 12:33:51 +0330

    feat: add locales config files

---
+++ src/constants/locales/en.ts
@@ -0,0 +1,10 @@
+import { PRODUCTS, PROJECTS, BLOG, CONTACT } from '../appRoutes';
+
+export const data = {
+  menu: [
+    { label: 'Products', link: PRODUCTS },
+    { label: 'projects', link: PROJECTS },
+    { label: 'blog', link: BLOG },
+    { label: 'contact', link: CONTACT },
+  ],
+};
---
+++ src/constants/locales/fa.ts
@@ -0,0 +1,10 @@
+import { PRODUCTS, PROJECTS, BLOG, CONTACT } from '../appRoutes';
+
+export const data = {
+  menu: [
+    { label: 'محصولات', link: PRODUCTS },
+    { label: 'پروژه‌ها', link: PROJECTS },
+    { label: 'بلاگ', link: BLOG },
+    { label: 'تماس‌ با ما', link: CONTACT },
+  ],
+};

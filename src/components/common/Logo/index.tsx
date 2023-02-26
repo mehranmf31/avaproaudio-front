7f712bfea1a378ec27316c934e0b8557bd87b6b5
Mehran <mehranmf31@gmail.com>
2023-02-26 20:02:57 +0330

    fix: modify logo component

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
-export interface NavbarProps {
+export interface LogoProps {
   width?: number;
   height?: number;
 }
 
-export const Logo = ({ width = 48, height = 48 }: NavbarProps): JSX.Element => {
+export const Logo = ({ width = 48, height = 48 }: LogoProps): JSX.Element => {
   return (
     <Image src="/logo192.png" width={width} height={height} alt="AVA Pro Audio logo" />
   );

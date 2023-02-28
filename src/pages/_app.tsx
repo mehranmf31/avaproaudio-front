ad621de465fe8713b755cef52faeb0d160939e66
Mehran <mehranmf31@gmail.com>
2023-02-28 20:09:29 +0330

    feat: add custom font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components/common/Layout';
-
-export default function App({ Component, pageProps }: AppProps) {
+import { Vazirmatn } from '@next/font/google';
+const vazir = Vazirmatn({ subsets: ['arabic', 'latin'] });
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --vazir-font: ${vazir.style.fontFamily};
+          }
+        `}
+      </style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    container: {
-      padding: {
-        DEFAULT: '1.5rem',
-        xl: '6rem',
-      },
-    },
-    extend: {},
+    container: { padding: { DEFAULT: '1.5rem', xl: '6rem' } },
+    extend: { fontFamily: { vazir: ['var(--vazir-font)', ...fontFamily.sans] } },
   },
   plugins: [],
 };
